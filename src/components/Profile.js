a0597b48f2fcfe8cedc477fafdb52b76517163de
Rishi Purwar <57035408+rishipurwar1@users.noreply.github.com>
2023-01-18 11:21:45 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,7 @@ const Profile = () => {
     <div className="profile">
       <h1 className="name">Rishi Purwar</h1>
       <h2 className="email">hello@world.com</h2>
+      <button>Logout</button>
     </div>
   );
 };
